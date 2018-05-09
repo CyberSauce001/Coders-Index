2fa7473ffb885d87dd9cd841b238de4a63b3d739
Ethereal15 <xionnguyen@gmail.com>
2018-05-08 22:10:35 -0700

    In progress

---
+++ Challenge-1/IntegerInput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n1, n2;
+  cout << "Enter a number: ";
+  cin >> n1;
+  cout << "\nEnter a number :";
+  cin >>n2;
+  
+  do{
+  }while();
+  return 0;
+  }
